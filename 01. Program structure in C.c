7616a5ff7e742985eb646e887f9e9f5dfff9b1a9
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-10-25 22:01:32 -0300

    Add files via upload

---
+++ 01. Program structure in C.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// la seccion de arriba son instruccioens del precompilador
+
+// todos los programas en C arrancan con la estructura de abajo, el "main"
+int main()
+{
+	printf("Hola Mundo!!!");
+    return 0;
+}
