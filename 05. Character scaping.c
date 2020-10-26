0c8948fe1390845c8b0626523dce3c33a5589f1a
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-10-26 03:51:15 -0300

    Add files via upload

---
+++ 05. Character scaping.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int miVariable=65;
+    float precio=10.50;
+    printf("Aca van dos lineas\n en un solo printf!\n\n");
+    printf("Y aca va la primera parte,");
+    printf("pero aca va la segunda\n");
+
+    printf("colu1\tcolu2\n");
+    printf("valor1\tvalor2\n");
+    printf("valor3\tvalor4\n");
+
+    printf("Las comillas: \"\n");
+    printf("La barra invertida: \\\n");
+    printf("La barra invertida: a\vb\n");
+
+
+    system("PAUSE");
+
+    return 0;
+}
